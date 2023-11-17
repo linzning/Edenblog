e7530fcf6b550a412a68fceae4feb2e09a199347
linzning <1274950922@qq.com>
2023-11-17 13:37:06 +0800

    认证过滤器

--- Eden-blog/src/main/java/org/Eden/config/SecurityConfig.java
+++ Eden-blog/src/main/java/org/Eden/config/SecurityConfig.java
@@ -1,5 +1,7 @@
 package org.Eden.config;
 
+import org.Eden.filter.JwtAuthenticationTokenFilter;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.security.authentication.AuthenticationManager;
@@ -8,6 +10,7 @@ import org.springframework.security.config.annotation.web.configuration.WebSecur
 import org.springframework.security.config.http.SessionCreationPolicy;
 import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 import org.springframework.security.crypto.password.PasswordEncoder;
+import org.springframework.security.web.authentication.UsernamePasswordAuthenticationFilter;
 
 @Configuration
 //WebSecurityConfigurerAdapter是Security官方提供的类
@@ -18,6 +21,10 @@ public class SecurityConfig extends WebSecurityConfigurerAdapter {
     public AuthenticationManager authenticationManagerBean() throws Exception {
         return super.authenticationManagerBean();
     }
+
+    @Autowired
+    //注入我们在huanf-blog工程写的JwtAuthenticationTokenFilter过滤器
+    private JwtAuthenticationTokenFilter jwtAuthenticationTokenFilter;
 
     @Bean
     //把官方的PasswordEncoder密码加密方式替换成BCryptPasswordEncoder
@@ -36,11 +43,20 @@ public class SecurityConfig extends WebSecurityConfigurerAdapter {
                 .authorizeRequests()
                 // 对于登录接口 允许匿名访问
                 .antMatchers("/login").anonymous()
+
+                //为方便测试认证过滤器，我们把查询友链的接口设置为需要登录才能访问。然后我们去访问的时候就能测试登录认证功能了
+                .antMatchers("/link/getAllLink").authenticated()
+
                 // 除上面外的所有请求全部不需要认证即可访问
                 .anyRequest().permitAll();
 
 
         http.logout().disable();
+
+        //把我们在huanf-blog工程写的JwtAuthenticationTokenFilter过滤器添加到Security的过滤器链中
+        //第一个参数是你要添加的过滤器，第二个参数是你想把你的过滤器添加到哪个security官方过滤器之前
+        http.addFilterBefore(jwtAuthenticationTokenFilter, UsernamePasswordAuthenticationFilter.class);
+
         //允许跨域
         http.cors();
     }
